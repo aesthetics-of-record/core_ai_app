3c0949e6f573a9dc476fcfbd185d9793ca94795e
aesthetics-of-record <aaaapple123@naver.com>
2023-12-05 17:36:53 +0900

    fix: 캐시 버그 수정 2

--- src/app/api/game/start/route.ts
+++ src/app/api/game/start/route.ts
@@ -2,14 +2,19 @@ import { apiOrigin } from "@/configs/urls";
 import axios from "axios";
 
 export async function GET(req: Request) {
-  const response: any = await axios.get(apiOrigin + "/api/game_start?count=4", {
-    headers: {
-      "Cache-Control": "no-cache, no-store, must-revalidate",
-      Pragma: "no-cache",
-      Expires: "0",
-    },
+  const response: any = await fetch(apiOrigin + "/api/game_start?count=4", {
+    cache: "no-store",
   });
+  const data = await response.json();
+
+  // await axios.get(apiOrigin + "/api/game_start?count=4", {
+  //   headers: {
+  //     "Cache-Control": "no-cache, no-store, must-revalidate",
+  //     Pragma: "no-cache",
+  //     Expires: "0",
+  //   },
+  // });
   // api/game_start 쿼리파라미터 count
 
-  return Response.json(response.data);
+  return Response.json(data);
 }
